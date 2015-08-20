81524ab8f3c56a30412356b7016c01512d9afdb3
Gaspar Teixeira <gaspar.teixeira@gmail.com>
2015-08-19 23:05:17 -0300

    ajustando elementos do layout e criando a navegacao

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,13 +6,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-      return $this->render('AppBundle:Default:index.html.twig');
+    public function indexAction(Request $request) {
+        $navigation = $this->getDoctrine()
+                ->getRepository('AppBundle:Navigation')
+                ->findAll();
+
+        return $this->render('AppBundle:Default:index.html.twig', array("navigation" => $navigation));
     }
+
 }
--- src/AppBundle/DataFixtures/ORM/LoadNavigationData.php
+++ src/AppBundle/DataFixtures/ORM/LoadNavigationData.php
@@ -12,12 +12,38 @@ class LoadNavigationData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $nav = new Navigation();
-        $nav->setName('Home');
-        $nav->setUrl('homepage');
-        $nav->setDescription("Página Inicial");
-
-        $manager->persist($nav);
+        $n1 = new Navigation();
+        $n1->setName('Home');
+        $n1->setUrl('/');
+        $n1->setIsPublic(TRUE);
+        $n1->setCreatedAt(new \DateTime('NOW'));
+        $manager->persist($n1);
+        $manager->flush();
+        
+        $n2 = new Navigation();
+        $n2->setName('Serviços');
+        $n2->setUrl("#");
+        $n2->setIsPublic(TRUE);
+        $n2->setCreatedAt(new \DateTime('NOW'));
+        $manager->persist($n2);
+        $manager->flush();
+        
+        $n3 = new Navigation();
+        $n3->setName('Anúncios');
+        $n3->setUrl('/anuncios');
+        $n3->setIsPublic(TRUE);
+        $n3->setCreatedAt(new \DateTime('NOW'));
+        $n3->setParent($n2);
+        $manager->persist($n3);
+        $manager->flush();
+        
+        $n4 = new Navigation();
+        $n4->setName('Parceiros');
+        $n4->setUrl('/parceiros');
+        $n4->setIsPublic(TRUE);
+        $n4->setCreatedAt(new \DateTime('NOW'));
+        $n4->setParent($n2);
+        $manager->persist($n4);
         $manager->flush();
     }
 }
